d880a688124fb0bdaaaefc6e7f57d2b27502f8da
luluaw4 <114546512+luluaw4@users.noreply.github.com>
2023-03-11 17:21:13 +0700

    MENAMBAHKAN FOLDER PRAKTIKUM 1

---
+++ Praktikum1-PW2/Praktikum/coba1.php
@@ -0,0 +1,8 @@
+<?php
+    define('PHI' , 3.14);
+    define('DBNAME', 'inventori');
+    define('DBSERVER', 'localhost');
+
+    echo 'Nilai Phi : ' . PHI;
+    echo '<br>Nama database nya : ' . DBNAME;
+    echo '<br> Lokasi Database nya : ' . DBSERVER;
---
+++ Praktikum1-PW2/Praktikum/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    $nama = 'Lulu'; 
+    $umur = 22;
+    $berat = 55;
+
+    echo 'Nama saya : ' . $nama;
+    echo '<br>Umur saya : ' . $umur;
+    echo '<br>Berat saya : ' . $berat;
+
+
+
+
+
+
+?>

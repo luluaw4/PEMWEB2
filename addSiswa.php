5a2e447c8f449cb477da0971191ba398488111ad
luluaw4 <luluaprilia1604@gmail.com>
2023-05-17 01:41:55 +0700

    MENAMBAHKAN FOLDER PRAKTIKUM 4

---
+++ addSiswa.php
@@ -0,0 +1,95 @@
+<?php
+    include_once 'template/header.php';
+?>
+
+<form>
+  <div class="form-group row">
+    <label for="text" class="col-4 col-form-label">Text Field</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-address-card"></i>
+          </div>
+        </div> 
+        <input id="text" name="text" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="textarea" class="col-4 col-form-label">Text Area</label> 
+    <div class="col-8">
+      <textarea id="textarea" name="textarea" cols="40" rows="5" class="form-control"></textarea>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="select" class="col-4 col-form-label">Select</label> 
+    <div class="col-8">
+      <select id="select" name="select" class="custom-select">
+        <option value="rabbit">Rabbit</option>
+        <option value="duck">Duck</option>
+        <option value="fish">Fish</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="select1" class="col-4 col-form-label">Select</label> 
+    <div class="col-8">
+      <select id="select1" name="select1" class="custom-select">
+        <option value="rabbit">Rabbit</option>
+        <option value="duck">Duck</option>
+        <option value="fish">Fish</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Checkboxes</label> 
+    <div class="col-8">
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="checkbox" id="checkbox_0" type="checkbox" checked="checked" class="custom-control-input" value="rabbit"> 
+        <label for="checkbox_0" class="custom-control-label">Rabbit</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="checkbox" id="checkbox_1" type="checkbox" class="custom-control-input" value="duck"> 
+        <label for="checkbox_1" class="custom-control-label">Duck</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="checkbox" id="checkbox_2" type="checkbox" class="custom-control-input" value="fish"> 
+        <label for="checkbox_2" class="custom-control-label">Fish</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Radio Buttons</label> 
+    <div class="col-8">
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="radio" id="radio_0" type="radio" class="custom-control-input" value="rabbit"> 
+        <label for="radio_0" class="custom-control-label">Rabbit</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="radio" id="radio_1" type="radio" class="custom-control-input" value="duck"> 
+        <label for="radio_1" class="custom-control-label">Duck</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="radio" id="radio_2" type="radio" class="custom-control-input" value="fish"> 
+        <label for="radio_2" class="custom-control-label">Fish</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="text4" class="col-4 col-form-label">Text Field</label> 
+    <div class="col-8">
+      <input id="text4" name="text4" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
+
+
+<?php
+    include_once 'template/footer.php';
+?>
